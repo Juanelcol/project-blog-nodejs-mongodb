328de1655fb0cd8cacb4a8c8823c717b51f52ab8
Juan Rodicio <juanelcol@gmail.com>
2022-03-29 12:35:34 -0300

    Update Usuario.js

--- models/Usuario.js
+++ models/Usuario.js
@@ -5,7 +5,7 @@ const Usuario = new Schema({
   nome: {type: String, required: true},
   email: {type: String, required: true},
   senha: {type: String, required: true},
-  //eAdmin: {type: Number, default: 0}
+  eAdmin: {type: Number, default: 0}
 })
 
 module.exports = mongoose.model('usuarios', Usuario)
